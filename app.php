cdad47b03ff1226c93ebc289016a20808537602d
isabelagoncalves1 <47746608+isabelagoncalves1@users.noreply.github.com>
2019-09-15 16:15:03 -0300

    Add files via upload

---
+++ app.php
@@ -0,0 +1,6 @@
+<?php
+
+define('CONTROLADOR_PADRAO', 'paginas'); //por padrão vem definido o controlador de paginas estáticas (paginasControlador)
+define('URL_BASE', 'http://localhost/loja/');
+//define('URL_BASE', 'http://90sclothes.epizy.com');
+//define('URL_BASE', 'https://clothes90s.000webhostapp.com/)
